e164ad50d8399814bb53a7fa8a545bd8b4a67535
Alexis Pinilla <alexispbarria@gmail.com>
2022-07-07 16:19:06 -0400

    Volviendo atrás

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,8 @@ const router = express.Router();
 const Participante = require('../models/Participante'); // LLAMANDO AL ESQUEMA CREADO "PARTICIPANTE.JS"
 
 
-router.get('/', async (req, res) => {
-    const cantidadInscritos = await Participante.aggregate([{$group: {_id: null, edadPromedio: {$avg: edadParticipante}}}, {$project: {_id: 0}}])
-    res.render('index', {cantidadInscritos});
+router.get('/', (req, res) => {
+    res.render('index');
 });
 
 router.get('/about', (req, res) => {
