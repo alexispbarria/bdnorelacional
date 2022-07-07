c9c39a4cccaf481392d968e0a592471e2e10f9d2
Alexis Pinilla <alexispbarria@gmail.com>
2022-07-07 16:15:01 -0400

    Promedio de edad

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Participante = require('../models/Participante'); // LLAMANDO AL ESQUEMA CREADO "PARTICIPANTE.JS"
 
-router.get('/', (req, res) => {
-    res.render('index');
+
+router.get('/', async (req, res) => {
+    const cantidadInscritos = await Participante.aggregate([{$group: {_id: null, edadPromedio: {$avg: edadParticipante}}}, {$project: {_id: 0}}])
+    res.render('index', {cantidadInscritos});
 });
 
 router.get('/about', (req, res) => {
     res.render('about')
 })
+
 
 module.exports = router;
